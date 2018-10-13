61d85a16b114a1a7805a2f244c0edd459888f722
davi-94 <davi.p.m94@gmail.com>
2018-10-12 23:01:44 -0300

    Remove Space

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min'
 import 'popper.js/dist/popper.min'
-import  'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/js/bootstrap.min'
 
 Vue.config.productionTip = false;
 
