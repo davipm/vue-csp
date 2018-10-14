556a3609b458209a4c08b772a62269ac8a896d24
davi-94 <davi.p.m94@gmail.com>
2018-10-14 19:08:54 -0300

    change script order, add bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery.min'
-import 'popper.js/dist/popper.min'
-import 'bootstrap/dist/js/bootstrap.min'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
