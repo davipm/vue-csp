35d8974994415025f85cf2cd7ea9a136d7173a94
davi-94 <davi.p.m94@gmail.com>
2018-10-14 19:10:09 -0300

    Bootstrap vue items

---
+++ bootstrap.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import bCarousel from 'bootstrap-vue/es/components/carousel/carousel';
+import bCarouselSlide from 'bootstrap-vue/es/components/carousel/carousel-slide'
+
+Vue.component('b-carousel', bCarousel);
+Vue.component('b-carousel-slide', bCarouselSlide);
