d20142e1a316b4e1b903e6460ca79e578122cc88
davi-94 <davi.p.m94@gmail.com>
2018-10-25 23:17:09 -0300

    Add alert component

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import bCarousel from 'bootstrap-vue/es/components/carousel/carousel';
 import bCarouselSlide from 'bootstrap-vue/es/components/carousel/carousel-slide'
+import bAlert from 'bootstrap-vue/es/components/alert/alert';
 
+Vue.component('b-alert', bAlert);
 Vue.component('b-carousel', bCarousel);
 Vue.component('b-carousel-slide', bCarouselSlide);
