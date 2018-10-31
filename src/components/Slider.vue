17ea0d31c5e016c514ece77dbe06b8e1de4711ca
davi-94 <davi.p.m94@gmail.com>
2018-10-30 23:20:33 -0300

    change carousel id

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div id="carouselHome" class="carousel slide carousel-fade" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item"
            v-for="(item, index) in images2"
@@ -15,11 +15,11 @@
       >
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
