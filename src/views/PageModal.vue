58fb6a7207fb034dbbda5a1ab46b32aec42cb383
davi-94 <davi.p.m94@gmail.com>
2018-10-31 22:56:59 -0300

    page modal template

---
+++ src/views/PageModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h2>Page Modal {{ $route.params.slug }} </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "PageModal",
+    data() {
+      return {
+
+      }
+    },
+
+    watch: {
+      '$route'(to, from) {
+        this.getRoute(to.params.slug);
+      }
+    },
+
+    methods: {
+      getRoute(slug) {
+        alert(`O slug é ${slug}`);
+      }
+    },
+
+    created() {
+      this.getRoute(this.$route.params.slug);
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
